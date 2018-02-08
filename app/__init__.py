cc53084a8b03f1815e4e88bde71cf00425a8acaf
Evgeniya Tveritinova <evgetv28@yandex.u>
2018-02-08 19:14:20 +0300

    add connect through socket

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask, jsonify
 from sqlalchemy import *
-from secure_info import user, password, host_m_1, port_m_1, host_m_2, port_m_2, host_s_1, port_s_1, host_s_2, port_s_2
+from secure_info import user, password, host_m_1, port_m_1, host_m_2, port_m_2, host_s_1, port_s_1, host_s_2, port_s_2,\
+    socket_m_1,socket_s_1,socket_m_2,socket_s_2
 
 app = Flask(__name__)
 #db = SQLAlchemy(app)
 
-engine_m_1 = create_engine('mysql://'+user+':'+password+'@'+host_m_1+':'+port_m_1+'/db1.db')
-engine_s_1 = create_engine('mysql://'+user+':'+password+'@'+host_s_1+':'+port_s_1+'/db1.db')
-engine_m_2 = create_engine('mysql://'+user+':'+password+'@'+host_m_2+':'+port_m_2+'/db1.db')
-engine_s_2 = create_engine('mysql://'+user+':'+password+'@'+host_s_2+':'+port_s_2+'/db1.db')
+engine_m_1 = create_engine('mysql://'+user+':'+password+'@'+host_m_1+':'+port_m_1+'/db1.db?unix_socket='+socket_m_1)
+engine_s_1 = create_engine('mysql://'+user+':'+password+'@'+host_s_1+':'+port_s_1+'/db1.db?unix_socket='+socket_s_1)
+engine_m_2 = create_engine('mysql://'+user+':'+password+'@'+host_m_2+':'+port_m_2+'/db1.db?unix_socket='+socket_m_2)
+engine_s_2 = create_engine('mysql://'+user+':'+password+'@'+host_s_2+':'+port_s_2+'/db1.db?unix_socket='+socket_s_2)
 
 metadata_m_1 = MetaData(engine_m_1)
 metadata_s_1 = MetaData(engine_s_1)
