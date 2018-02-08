2ad139650b83e580e1b559fdca1053b4e0c4d941
Evgeniya Tveritinova <evgetv28@yandex.u>
2018-02-09 02:53:30 +0300

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 import json
 from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
 from secure_info import user, password, host_m_1, port_m_1, host_m_2, port_m_2, host_s_1, port_s_1, host_s_2, port_s_2,\
     socket_m_1,socket_s_1,socket_m_2,socket_s_2
 
@@ -18,6 +19,7 @@ metadata_s_1 = MetaData(engine_s_1)
 metadata_m_2 = MetaData(engine_m_2)
 metadata_s_2 = MetaData(engine_s_2)
 
+session_s_1 = sessionmaker(bind=engine_s_1)
 
 games_m_1 = Table('games', metadata_m_1, autoload=True)
 games_s_1 = Table('games', metadata_s_1, autoload=True)
@@ -40,5 +42,5 @@ def root():
 @app.route('/games', methods=['GET'])
 def get_games():
     #games_s_1.select().execute()
-    print games_s_1.query().all()
+    print session_s_1.query(games_s_1).all()
     return jsonify({'hey':'you'}), 200
