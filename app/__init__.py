165944e7c89f9d743f55b05eaa899a79ed70e927
Evgeniya Tveritinova <evgetv28@yandex.u>
2018-02-09 15:08:26 +0300

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -82,7 +82,7 @@ def get_game(game_id):
     try:
         res_data = cur['session'].query(cur['games']).filter(cur['games'].c.id == game_id).one()
     except NoResultFound:
-        return ('',204)
+        return '', 204
     return jsonify(get_json(res_data, get_cols(cur['session'], cur['games']))), 200
 
 
@@ -102,5 +102,5 @@ def put_teams(game_id):
     country = request.args.get('country')
 
     cur['session'].execute(cur['teams'].insert().values(name=name, country=country))
-
-    return ('', 200)
+    cur['session'].commit()
+    return 'success', 200
