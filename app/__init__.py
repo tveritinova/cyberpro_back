4897d9a55db71da4e1d4c4119f148c1e4464510f
Evgeniya Tveritinova <evgetv28@yandex.u>
2018-02-10 11:27:18 +0300

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -83,7 +83,7 @@ def create():
     def post_teams(game_id):
         cur = data[choose(game_id)]['m']
 
-        raise Exception(request.stream.read())
+        raise Exception(request.args)
 
         name = request.get_data().get('name')
         country = request.data.get('country')
