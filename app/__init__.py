272257b3ff4f37f721542924e14afc0142fb4d1b
Evgeniya Tveritinova <evgetv28@yandex.u>
2018-02-11 15:19:13 +0300

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -21,7 +21,7 @@ def choose(game_id):
 
 def get_json(instance, exc):
     res_dict = {}
-    cols = instance.__class__.keys()
+    cols = instance.__class__.columns.keys()
     for i in range(len(cols)):
         if cols[i] not in exc:
             res_dict[cols[i]] = getattr(instance, cols[i])
