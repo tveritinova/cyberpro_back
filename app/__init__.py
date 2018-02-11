567478f50a8c5ada68331476a1e02878efdcdaaa
Evgeniya Tveritinova <evgetv28@yandex.u>
2018-02-11 15:07:59 +0300

    all tests + refactor with automap

--- app/__init__.py
+++ app/__init__.py
@@ -19,20 +19,13 @@ def choose(game_id):
     return game_id % 2
 
 
-def get_cols(session, table):
-    return [col['name'] for col in session.query(table).column_descriptions]
-
-
-def get_json(tuple, cols, exc):
+def get_json(instance, exc):
     res_dict = {}
+    cols = instance.__class__.c.keys()
     for i in range(len(cols)):
         if cols[i] not in exc:
-            res_dict[cols[i]] = tuple[i]
+            res_dict[cols[i]] = getattr(instance, cols[i])
     return res_dict
-
-
-def get_row(id, table, session):
-    return session.query(table).filter(table.c.id == id).one()
 
 
 def create(testing=False, debug=False):
@@ -45,15 +38,17 @@ def create(testing=False, debug=False):
         for r in ['m', 's']:
             data[i][r]['engine'] = create_engine('mysql://'+user+':'+password+'@'+host[i][r]+':'+port[i][r] +
                                                  '/db'+str(i+1)+'?unix_socket='+socket[i][r])
-            data[i][r]['metadata'] = MetaData(data[i][r]['engine'])
+            #data[i][r]['metadata'] = MetaData(data[i][r]['engine'])
             data[i][r]['session'] = sessionmaker(bind=data[i][r]['engine'])()
-            data[i][r]['games'] = Table('games', data[i][r]['metadata'], autoload=True)
-            data[i][r]['games_exc'] = ['img_path']
-            data[i][r]['teams'] = Table('teams', data[i][r]['metadata'], autoload=True)
-            data[i][r]['teams_exc'] = ['img_path']
+            #data[i][r]['games'] = Table('games', data[i][r]['metadata'], autoload=True)
+            #data[i][r]['games_exc'] = ['img_path']
+            #data[i][r]['teams'] = Table('teams', data[i][r]['metadata'], autoload=True)
+            #data[i][r]['teams_exc'] = ['img_path']
 
             data[i][r]['base'] = automap_base()
             data[i][r]['base'].prepare(data[i][r]['engine'], reflect=True)
+
+    exc_img_path = ['img_path']
 
     @app.route('/')
     def root():
@@ -62,29 +57,33 @@ def create(testing=False, debug=False):
     @app.route('/games', methods=['GET'])
     def get_games():
         cur = data[0]['s' if not testing else 'm']
-        res_data = cur['session'].query(cur['games']).all()
-        res_dict = [get_json(game, get_cols(cur['session'],cur['games']),cur['games_exc']) for game in res_data]
+        class_ = cur['base'].classes.games
+        res_data = class_.query.all()
+        res_dict = [get_json(game, exc_img_path) for game in res_data]
         return jsonify(games=res_dict), 200
 
     @app.route('/games/<int:game_id>', methods=['GET'])
     def get_game(game_id):
         cur = data[0]['s' if not testing else 'm']
+        games = cur['base'].classes.games
         try:
-            res_data = cur['session'].query(cur['games']).filter(cur['games'].c.id == game_id).one()
+            res_data = games.query.get(game_id)
         except NoResultFound:
             return '', 204
-        return jsonify(get_json(res_data, get_cols(cur['session'], cur['games']), cur['games_exc'])), 200
+        return jsonify(get_json(res_data, exc_img_path)), 200
 
     @app.route('/games/<int:game_id>/teams', methods=['GET'])
     def get_teams(game_id):
         cur = data[choose(game_id)]['s' if not testing else 'm']
-        res_data = cur['session'].query(cur['teams']).filter(cur['teams'].c.game_id == game_id).all()
-        res_dict = [get_json(game, get_cols(cur['session'], cur['teams']), cur['teams_exc']) for game in res_data]
+        teams = cur['base'].classes.teams
+        res_data = teams.query.filter(teams.game_id == game_id).all()
+        res_dict = [get_json(game, exc_img_path) for game in res_data]
         return jsonify(teams=res_dict), 200
 
     @app.route('/games/<int:game_id>/teams', methods=['POST'])
     def post_teams(game_id):
         cur = data[choose(game_id)]['m']
+        teams = cur['base'].classes.teams
 
         name = ast.literal_eval(request.data).get('name')
         country = ast.literal_eval(request.data).get('country')
@@ -93,9 +92,7 @@ def create(testing=False, debug=False):
             return 'Missed name, country data', 400
 
         base = cur['base']
-        cur['session'].add(base.classes.teams(name=name, country=country, game_id=game_id))
-
-        # catch failed unique cons
+        cur['session'].add(teams(name=name, country=country, game_id=game_id))
 
         try:
             if testing:
@@ -106,19 +103,29 @@ def create(testing=False, debug=False):
             if e.orig[0] == 1062:
                 return 'Name unique constraint failed', 400
 
-        res_data = cur['session'].query(cur['teams']).filter(cur['teams'].c.name == name).one()
+        res_data = teams.query.filter(cur['teams'].c.name == name).one()
 
-        return jsonify(get_json(res_data, get_cols(cur['session'],cur['teams']), cur['teams_exc'])), 201
+        return jsonify(get_json(res_data, cur['teams_exc'])), 201
 
     @app.route('/games/<int:game_id>/teams/<int:team_id>', methods=['GET'])
     def get_team(game_id, team_id):
         cur = data[choose(game_id)]['s' if not testing else 'm']
+        teams = cur['base'].classes.teams
 
         try:
-            res_data = cur['session'].query(cur['teams']).filter(cur['teams'].c.id == team_id).one()
+            res_data = teams.get(team_id)
         except NoResultFound:
             return '', 204
 
-        return jsonify(get_json(res_data, get_cols(cur['session'], cur['teams']), cur['teams_exc'])), 200
+        return jsonify(get_json(res_data, exc_img_path)), 200
+
+    @app.route('/games/<int: game_id>/teams/<int: team_id>/players', methods=['GET'])
+    def get_players(game_id, team_id):
+        cur = data[choose(game_id)]['s' if not testing else 'm']
+        c = cur['base'].classes.players
+
+        res_data = c.query.all()
+        res_dict = [get_json(player) for player in res_data]
+        return jsonify(games=res_dict), 200
 
     return app
--- test_api.py
+++ test_api.py
@@ -2,10 +2,11 @@ import unittest
 from app import create
 import ast
 import json
+import datetime
 
 
 class BackApiTestCase(unittest.TestCase):
-    """This class represents the bucketlist test case"""
+    """This class represents the api test case"""
 
     def setUp(self):
         """Define test variables and initialize app."""
@@ -16,15 +17,23 @@ class BackApiTestCase(unittest.TestCase):
                       {'name': 'lol', 'id': 3},
                       {'name': 'hearthstone', 'id': 4},
                       {'name': 'overwatch', 'id': 5}]
+        self.first_team = {'name': 'team1', 'country': 'russia'}
+        self.second_team = {'name': 'team2', 'country': 'russia'}
+        self.player = {'name': 'Test Player', 'nickname': 'test_player', 'country': 'China', 'is_cap': False}
+        self.tournament = {'name': 'test tournament', 'start_date': datetime.date(2018, 2, 11),
+                           'end_date': datetime.date(2018, 2, 12)}
+        self.match = {'num_in_stage': 1, 'date': datetime.datetime(2018, 2, 11, 11)}
+        self.transaction = {'date': datetime.date(2018, 2, 12)}
+        self.headers_to_post = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 
     def test_api_can_get_all_games(self):
-        """Test API can get a bucketlist (GET request)."""
+        """Test API can get games. (GET request)"""
         res = self.client().get('/games')
         self.assertEqual(res.status_code, 200)
         self.assertEqual(ast.literal_eval(res.data)['games'], self.games)
 
     def test_api_can_get_game_by_id(self):
-        """Test API can get a single bucketlist by using it's id."""
+        """Test API can get a single game by using it's id. (GET request)"""
         result = self.client().get('/games/1')
         self.assertEqual(result.status_code, 200)
         self.assertEqual(ast.literal_eval(result.data)['id'], 1)
@@ -34,24 +43,127 @@ class BackApiTestCase(unittest.TestCase):
         result = self.client().get('/games/6')
         self.assertEqual(result.status_code, 204)
 
-    def test_api_can_post_team(self):
-        """Test API can edit an existing bucketlist. (PUT request)"""
-        data_to_put = {'name': 'team1', 'country': 'russia'}
-        rv = self.client().post('/games/1/teams',
-                                data=json.dumps(data_to_put),
-                                headers={'Content-Type':'application/json',
-                                         'Accept':'application/json'})
+    def test_api_can_get_team(self):
+        """Test API can get a team by id. (GET request)"""
+        result = self.client().post('/games/1/teams',
+                                    data=json.dumps(self.first_team), headers=self.headers_to_post)
+        self.first_team_id = ast.literal_eval(result.data)['id']
+        result = self.client().get('/games/1/teams/'+ str(self.first_team_id))
+        self.assertEqual(result.status_code, 200)
+        self.assertEqual(ast.literal_eval(result.data)['name'], self.first_team['name'])
+        self.assertEqual(ast.literal_eval(result.data)['country'], self.first_team['country'])
 
-        self.assertEqual(rv.status_code, 201)
-        results = self.client().get('/games/1/teams/'+str(ast.literal_eval(rv.data)['id']))
-        self.assertEqual(results.status_code, 200)
-        self.assertEqual(ast.literal_eval(results.data)['name'], data_to_put['name'])
-        self.assertEqual(ast.literal_eval(results.data)['country'], data_to_put['country'])
+
+    def test_api_can_post_team(self):
+        """Test API can add a team. (POST request)"""
         rv = self.client().post('/games/1/teams',
-                                data=json.dumps(data_to_put),
-                                headers={'Content-Type':'application/json',
-                                         'Accept':'application/json'})
+                                data=json.dumps(self.second_team), headers=self.headers_to_post)
+        self.assertEqual(rv.status_code, 201)
+        self.second_team_id = ast.literal_eval(rv.data)['id']
+        results = self.client().get('/games/1/teams/'+str(self.second_team_id))
+        self.assertEqual(results.status_code, 200)
+        self.assertEqual(ast.literal_eval(results.data)['name'], self.second_team['name'])
+        self.assertEqual(ast.literal_eval(results.data)['country'], self.second_team['country'])
+        rv = self.client().post('/games/1/teams',
+                                data=json.dumps(self.second_team), headers=self.headers_to_post)
         self.assertEqual(rv.status_code, 400)
+        result = self.client().get('/games/1/teams/'+str(ast.literal_eval(rv.data)['id']+1))
+        self.assertEqual(result.status_code, 404)
+
+    def test_api_can_post_and_get_all_players(self):
+        """Test API can get all players by team. (GET request)"""
+        self.player['team_id'] = self.first_team_id
+        result = self.client().post('/games/1/teams/1/players',
+                                    data=json.dumps(self.player), headers=self.headers_to_post)
+        self.player_id = ast.literal_eval(result.data)['id']
+        self.assertEqual(result.status_code, 201)
+        result = self.client().get('/games/1/teams/1/players/')
+        self.assertEqual(result.status_code, 200)
+
+    def test_api_can_get_player(self):
+        """Test API can get a player by team. (GET request)"""
+        result = self.client().get('/games/1/teams/1/players'+str(self.player_id))
+        self.assertEqual(result.status_code, 200)
+        data = ast.literal_eval(result.data)
+        self.assertEqual(data['name'], self.player['name'])
+        self.assertEqual(data['nickname'], self.player['nickname'])
+        self.assertEqual(data['country'], self.player['country'])
+        self.assertEqual(data['team_id'], self.player['team_id'])
+        self.assertEqual(data['is_cap'], self.player['is_cap'])
+
+    def test_api_can_post_and_get_tournaments_and_one(self):
+        """Test API can get all tournaments and one by id. (GET request)"""
+        result = self.client().post('/tournaments', data=self.tournament, headers=self.headers_to_post)
+        self.assertEqual(result.status_code, 201)
+        self.tournament_id = ast.literal_eval(result.data)['id']
+
+        result = self.client().get('/tournaments')
+        self.assertEqual(result.status_code, 200)
+
+        result = self.client().get('/tournaments/'+str(self.tournament_id))
+        self.assertEqual(result.status_code, 200)
+        data = ast.literal_eval(result.data)
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data['name'], self.tournament['name'])
+        self.assertEqual(data['start_date'], len(self.tournament['start_date']))
+        self.assertEqual(data['end_date'], len(self.tournament['end_date']))
+
+    def test_matches(self):
+        self.match['tournament_id'] = self.tournament_id
+        self.match['first_team_id'] = self.first_team_id
+        self.match['second_team_id'] = self.second_team_id
+
+        result = self.client().post('/matches', data=self.match, headers=self.headers_to_post)
+        self.assertEqual(result.status_code, 201)
+        self.match_id = ast.literal_eval(result.data)['id']
+
+        result = self.client().get('/matches/')
+        self.assertEqual(result.status_code, 200)
+
+        result = self.client().get('/matches/' + str(self.match_id))
+        self.assertEqual(result.status_code, 200)
+        data = ast.literal_eval(result.data)
+        self.assertEqual(data['id'], self.match_id)
+        self.assertEqual(data['tournament_id'], self.match['tournament_id'])
+        self.assertEqual(data['first_team_id'], self.match['first_team_id'])
+        self.assertEqual(data['second_team_id'], self.match['second_team_id'])
+        self.assertEqual(data['num_in_stage'], self.match['num_in_stage'])
+        self.assertEqual(data['date'], str(self.match['date']))
+
+    def test_players_transactions(self):
+        self.transaction['player_id'] = self.player_id
+        self.transaction['from_team_id'] = self.first_team_id
+        self.transaction['to_team_id'] = self.second_team_id
+
+        result = self.client().post('/transactions', data=self.transaction, header=self.headers_to_post)
+        self.assertEqual(result.status_code, 201)
+        self.transaction_id = ast.literal_eval(result.data)['id']
+
+        result = self.client().get('/transactions/'+str(self.transaction_id))
+        self.assertEqual(result.status_code, 200)
+        data = ast.literal_eval(result.data)
+        self.assertEqual(data['player_id'], self.transaction['player_id'])
+        self.assertEqual(data['from_team_id'], self.transaction['from_team_id'])
+        self.assertEqual(data['to_team_id'], self.transaction['to_team_id'])
+        self.assertEqual(data['date'], self.transaction['date'])
+
+        result = self.client().get('/transactions')
+        self.assertEqual(result.status_code, 200)
+
+    def test_team_in_tournament(self):
+        result = self.client().post('/tournaments', data={'tournament_id': self.tournament_id,
+                                                          'team_id': self.first_team_id})
+        self.assertEqual(result.status_code, 201)
+
+        result = self.client().get('/tournaments/'+str(self.tournament_id)+'/teams')
+        self.assertEqual(result.status_code, 200)
+        data = ast.literal_eval(result.data)
+        self.assertIn(data['teams_id'], self.first_team_id)
+
+        result = self.client().get('/teams/'+str(self.first_team_id)+'/tournaments')
+        self.assertEqual(result.status_code, 200)
+        data = ast.literal_eval(result.data)
+        self.assertIn(data['tournaments_id'], self.tournament_id)
 
     def tearDown(self):
         """teardown all initialized variables."""
